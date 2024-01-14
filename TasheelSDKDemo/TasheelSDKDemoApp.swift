c202a2b1905bbaf772b17eda565263ea9506bd4e
Hamad Fuad <ihamadfouad@icloud.com>
2024-01-14 14:17:50 +0300

    Initial Commit

---
+++ TasheelSDKDemo/TasheelSDKDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TasheelSDKDemoApp.swift
+//  TasheelSDKDemo
+//
+//  Created by Hamad Fuad on 14/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TasheelSDKDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
