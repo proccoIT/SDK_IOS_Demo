4e207f8d1bcf57e4e7bbe536ca25c170c045818b
Hamad Fuad <ihamadfouad@icloud.com>
2024-04-24 10:52:10 +0300

    Init commit

--- TasheelSDKDemo/ContentView.swift
+++ TasheelSDKDemo/ContentView.swift
@@ -6,19 +6,29 @@
 //
 
 import SwiftUI
+import TasheelSDK
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  
+  init() {
+    do {
+      try Tasheel.assignMerchantIdentifier("1234567890")
+    } catch let error {
+      print(error)
     }
+  }
+  
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
